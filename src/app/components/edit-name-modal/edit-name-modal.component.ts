496217f4b97d206b682e692be509aac15bc9c001
Ali Ibrahim <ai7417916@gmail.com>
2025-04-10 18:45:01 +0300

    fixing the edit name modal design

--- src/app/components/edit-name-modal/edit-name-modal.component.ts
+++ src/app/components/edit-name-modal/edit-name-modal.component.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, Inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-edit-name-modal',
   standalone: true,
-  imports: [CommonModule,MatDialogModule,FormsModule],
+  imports: [CommonModule,MatDialogModule,FormsModule,MatButtonModule,MatInputModule],
   templateUrl: './edit-name-modal.component.html',
   styleUrl: './edit-name-modal.component.scss'
 })
